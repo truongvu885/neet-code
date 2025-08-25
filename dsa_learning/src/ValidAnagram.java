4537035497e2c050064b5ca86bb69c4195010c25
truongvu885 <75032594+truongvu885@users.noreply.github.com>
2025-08-25 10:51:46 +0000

    commit Valid anagram

---
+++ dsa_learning/src/ValidAnagram.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Map;
+//https://neetcode.io/problems/is-anagram?list=allNC
+/**
+ * convert string to char arrays
+ * sort arrays
+ * compare two array
+ * time complexity: O(nlogn + mlogm)
+ * space complexity; O(1) or O(n+m) depending on the sorting algorithm
+ */
+public class ValidAnagram {
+    public static void main(String [] args){
+        System.out.println(isAnagram("anagram", "aanrgam"));
+    }
+
+    public static boolean isAnagram(String s, String t){
+        if(s.length() != t.length())
+            return false;
+
+        char[] sChars = s.toCharArray();
+        char[] tChars = s.toCharArray();
+
+       
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        System.out.println(sChars);
+        System.out.println(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+
+}
---
+++ dsa_learning/src/ValidAnagram_2.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * put each character and quantity to the Map
+ * compare two Map
+ * time complexity: O(n+m)
+ * space complexity: O(1)
+ */
+public class ValidAnagram_2 {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "anagrma"));
+    }
+
+    private static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0)+ 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0)+ 1);
+        }
+        System.out.println(sMap);
+        System.out.println(tMap);
+        return sMap.equals(tMap);
+    }
+
+
+}
